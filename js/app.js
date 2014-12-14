98da3f4705b1adcf55238d47beb25f39909e9954
Daniel Nelmes-Love <dnelmeslove@gmail.com>
2014-12-14 22:10:40 +1300

    added 'Add Item' button and set button interaction; added 'Retern/Enter' keyDown interaction; updated size of list items to match buttons; shrunk size of cart and delete buttons to match li and hid these imgs; added mouseenter/mouseleave for list imgs; added placeholder error when no item input

--- js/app.js
+++ js/app.js
@@ -1,21 +1,47 @@
 $(document).ready(function() {
 
-	$('.cart').on('click', function() {
-		if ($(this).closest('li').hasClass('in-cart')) {
-			$(this).closest('li').removeClass('in-cart');
+	$('#add-button').on('click', function() {
+		if ($('#new-item').val().trim()==0) {
+			$('#new-item').attr('placeholder', "Please input an item to add to the list");
+			$('#new-item').addClass('list-error')
 		}
 		else {
-			$(this).closest('li').addClass('in-cart');
-		};	
+		$('#list').append(
+			'<li><img class="delete-item" src="img/delete-item.png" alt="Delete Item">' +
+			document.getElementById('new-item').value +
+			'<img class="cart" src="img/cart.png" alt="Cart"></li>'
+		);
+		$("#new-item").attr('placeholder', "Type items here then push 'Enter' on your keyboard");
+		$('#new-item').removeClass('list-error')
+		$('#new-item').val('');
+		};
 	});
 
-	$('.delete-item').on('click', function() {
-		$(this).closest('li').remove();
+	$(this).on('keydown', function(e) {
+		var newItem = (e.keyCode, e.which == 13)
+		if (newItem) {
+			$('#add-button').click();
+		};
 	});
 
 	$('#clear-button').on('click', function() {
 		$('#list').find('li').remove();
 	});
 
+	$('#list').on('click', '.cart', function() {
+		$(this).closest('li').toggleClass('in-cart');
+	});
+
+	$('#list').on('click', '.delete-item', function() {
+		$(this).parent().remove();
+	});
+
+	$('#list').on('mouseenter', 'li', function() {
+		$(this).find('img').show();
+	});
+
+	$('#list').on('mouseleave', 'li', function() {
+		$(this).find('img').hide();
+	});
 
 });
