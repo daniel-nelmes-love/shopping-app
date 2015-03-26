a893329a109840f2bfc5716078e236d55924ad87
Daniel Nelmes-Love <dnelmeslove@gmail.com>
2015-03-27 02:48:31 +1300

    v1
    added array and means to populate it.
    need to be able to search array for toggle.

--- js/app.js
+++ js/app.js
@@ -1,26 +1,42 @@
 $(document).ready(function() {
-	addItem()
+var itemNumber = 0
+var items = []
+
+	addItem(itemNumber, items)
 	returnKey()
 	mouseActions()
-	toggleItem()
+	toggleItem(items)
 	removeItem()
+	clearList()
 });
 
-function addItem() {
+function addItem(itemNumber, items) {
 	$('#add-button').on('click', function() {
 		if ($('#new-item').val().trim()==0) {
 			$('#new-item').attr('placeholder', "Please input an item to add to the list");
 			$('#new-item').addClass('list-error')
 		}
 		else {
-		$('#list').append(
-			'<li><img class="delete-item" src="img/delete-item.png" alt="Delete Item">' +
-			document.getElementById('new-item').value +
-			'<img class="cart" src="img/cart.png" alt="Cart"></li>'
-		);
-		$("#new-item").attr('placeholder', 'Type items here then push Enter on your keyboard');
-		$('#new-item').removeClass('list-error')
-		$('#new-item').val('');
+			itemNumber++
+			$('#list').append(
+				'<li value="item' + itemNumber + '"><img class="delete-item" src="img/delete-item.png" alt="Delete Item">' +
+				document.getElementById('new-item').value +
+				'<img class="cart" src="img/cart.png" alt="Cart"></li>'
+			);
+			$("#new-item").attr('placeholder', 'Type items here then push Enter on your keyboard');
+			$('#new-item').removeClass('list-error')
+
+			
+			var item = {}
+			item.id = "item" + itemNumber
+			item.name = document.getElementById('new-item').value
+			item.toggle = "On"
+			items.push(item);
+			console.log(items);
+
+			$('#new-item').val('');
+			console.log(itemNumber);
+
 		};
 	});
 }
@@ -44,18 +60,43 @@ function mouseActions() {
 	});
 }
 
-function toggleItem() {
+function toggleItem(items) {
 	$('#list').on('click', '.cart', function() {
+		function findIndex(selectedItem) {
+			for (var i = 0; i < items.length; i++) {
+				if (items[i].id = selectedItem) {
+					return i - 1
+				};
+			};
+		}
+
+		var selectedItem = $(this).parent().attr('value');
+		findIndex(selectedItem);
+		console.log(items[findIndex]);
+
+		//if (items[index].toggle="On") {
+		//	items[index].toggle="Off"
+		//} else {
+		//	items[index].toggle="On"
+		//}
+		//console.log(items(tempItem.toggle));
+
+
 		$(this).closest('li').toggleClass('in-cart');
 	});
+	//var tempItem = $.grep(items, function(e){return e.id==id;});
 
+}
+
+function removeItem() {
 	$('#list').on('click', '.delete-item', function() {
 		$(this).parent().remove();
 	});
 }
 
-function removeItem() {
+function clearList() {
 	$('#clear-button').on('click', function() {
 		$('#list').find('li').remove();
 	});
+	itemNumber = 0
 }
