a1fd1057186c0b8a7596112181f78f55e9418fc5
Daniel Nelmes-Love <dnelmeslove@gmail.com>
2015-03-27 01:04:15 +1300

    Cleanup

--- js/app.js
+++ js/app.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
+	addItem()
+	returnKey()
+	mouseActions()
+	toggleItem()
+	removeItem()
+});
 
+function addItem() {
 	$('#add-button').on('click', function() {
 		if ($('#new-item').val().trim()==0) {
 			$('#new-item').attr('placeholder', "Please input an item to add to the list");
@@ -16,26 +23,18 @@ $(document).ready(function() {
 		$('#new-item').val('');
 		};
 	});
+}
 
+function returnKey() {
 	$(this).on('keydown', function(e) {
 		var newItem = (e.keyCode, e.which == 13)
 		if (newItem) {
 			$('#add-button').click();
 		};
 	});
+}
 
-	$('#clear-button').on('click', function() {
-		$('#list').find('li').remove();
-	});
-
-	$('#list').on('click', '.cart', function() {
-		$(this).closest('li').toggleClass('in-cart');
-	});
-
-	$('#list').on('click', '.delete-item', function() {
-		$(this).parent().remove();
-	});
-
+function mouseActions() {
 	$('#list').on('mouseenter', 'li', function() {
 		$(this).find('img').show();
 	});
@@ -43,5 +42,20 @@ $(document).ready(function() {
 	$('#list').on('mouseleave', 'li', function() {
 		$(this).find('img').hide();
 	});
+}
 
-});
+function toggleItem() {
+	$('#list').on('click', '.cart', function() {
+		$(this).closest('li').toggleClass('in-cart');
+	});
+
+	$('#list').on('click', '.delete-item', function() {
+		$(this).parent().remove();
+	});
+}
+
+function removeItem() {
+	$('#clear-button').on('click', function() {
+		$('#list').find('li').remove();
+	});
+}
